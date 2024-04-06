669c32fdf1f2d7dd181865640882cc768395141d
Gabriel dos Santos <sga91000@gmail.com>
2024-04-06 16:00:05 -0300

    Primeiros comandos do javascript

--- scripts/controler/CalcController.js
+++ scripts/controler/CalcController.js
@@ -1,26 +1,62 @@
 class CalcController {
 
-    constructor(){
+    constructor() {
 
-        this._displayCalc ="0";
-        this._dataAtual;
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+        this.initialize();
     }
 
-    get displayCalc(){
+    initialize() {
 
-        return this._displayCalc;
+        this.setDisplayDateTime();
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor; 
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get displayTime() {
+        return this._timeEl.innerHTML;
     }
 
-    set dataAtual(variavel){
-        this._dataAtual = variavel;
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc() {
+
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
     }
 
 }
