011565b3494baf26bc9bef05520622d5900bb9d0
gabrielglasser <sga91000@gmail.com>
2024-09-06 00:50:58 -0300

    Drag e cursor pointer

--- scripts/controler/CalcController.js
+++ scripts/controler/CalcController.js
@@ -18,17 +18,29 @@ class CalcController {
         }, 1000);
     }
 
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+
+    };
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach((btn, index)=>{
-                btn.addEventListener('click', e =>{
+                this.addEventListenerAll(btn, 'click drag', e =>{
 
                 console.log(e);
     
-            })
-        })
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+        });
 
     }
 
